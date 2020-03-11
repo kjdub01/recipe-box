738b96c19362b78eda653082df8e084be19376d0
kjdub01 <kjewettwilliams@gmail.com>
2020-03-11 10:48:16 -0700

    update routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :measurement_qties
-  resources :measurement_units
-  resources :ingredients
+  #resources :ingredients
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
