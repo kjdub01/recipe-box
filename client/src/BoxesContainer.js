1de1e087c53bfb1c28e2e19750e714f479118ae0
kjdub01 <kjewettwilliams@gmail.com>
2020-03-27 11:45:00 -0700

    create basic structure

---
+++ client/src/BoxesContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class BoxesContainer extends React.Component {
+
+    render() {
+        return(
+            <MainHeader />
+            <MainFooter />
+        )
+    }
+}
