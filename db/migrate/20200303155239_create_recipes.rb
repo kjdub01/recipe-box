e99cc7e6fb72d75b7002697d55f88e8440766381
kjdub01 <kjewettwilliams@gmail.com>
2020-05-14 11:02:35 -0700

    add img links to recipe model

--- db/migrate/20200303155239_create_recipes.rb
+++ db/migrate/20200303155239_create_recipes.rb
@@ -2,6 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :recipe_name
+      t.string :img
       t.text :recipe_description
       t.text :recipe_direction
 
