c5c97755ef72ff8ef46441acadb958d52c0b7fc1
kjdub01 <kjewettwilliams@gmail.com>
2020-05-15 18:35:36 -0700

    move fetch call to this top level component

--- client/src/BoxesContainer.js
+++ client/src/BoxesContainer.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import styles from './App.css'
 import MainHeader from './MainHeader'
-import MainFooter from './MainFooter';
+import MainFooter from './MainFooter'
+import TileIndex from './TileIndex';
 
 class BoxesContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/recipes')
+            .then(response => {return response.json()})
+            .then(data => {this.setState({ recipes: data }) });
+    }
 
     render() {
         return(
             <div>
                 <MainHeader />
+                <TileIndex
+                    recipes={this.state.recipies}
+                    //show={this.showBox.bind(this)}
+                />
                 <MainFooter />
             </div>
-        )
+        );
     }
 }
 
