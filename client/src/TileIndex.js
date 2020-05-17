252258dde72533ab4bb5eeff14f4a32e89db65ab
kjdub01 <kjewettwilliams@gmail.com>
2020-05-17 14:05:08 -0700

    delete old code and pass props to Tile component

--- client/src/TileIndex.js
+++ client/src/TileIndex.js
@@ -3,34 +3,11 @@ import styles from './App.css'
 import Tile from './Tile'
 
 class TileIndex extends React.Component {
-
-    /*constructor(props) {
-        super(props);
-        this.state = {
-            recipes: []
-        }
-    }
-
-    componentDidMount() {
-        fetch('/recipes')
-            .then(response => {return response.json()})
-            .then(data => {this.setState({ recipes: data }) });
-    }*/
-
-
     render() {
-        const recipes = this.props.recipes.map((recipe) => {
-            return(
-                <div key ={recipe.id}>
-                    <h1>{recipe.recipe_name}</h1>
-                    <p>{recipe.img}</p>
-                </div>
-            );
-        })
         return(
             <div className='container'>
                 <main className="tile-container">
-                    {recipes}
+                    <Tile recipes={this.props.recipes} />
                 </main>
             </div>
         )    
