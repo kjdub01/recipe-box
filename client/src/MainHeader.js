ced9dc0c4d2939c8cb813434f786c9b90bf8e0e1
kjdub01 <kjewettwilliams@gmail.com>
2020-05-24 15:21:16 -0700

    put add button in and create toggle hidden function for add recipe form

--- client/src/MainHeader.js
+++ client/src/MainHeader.js
@@ -1,14 +1,25 @@
 import React from 'react'
 //import styles from './App.scss'
-
+import AddButton from './AddButton.js'
 
 class MainHeader extends React.Component {
+    
+    toggleHidden() {
+        if (this.state.isHidden ) {
+            this.setState({isHidden: false})
+        } else {
+            this.setState({isHidden: true})
+        }
+    }
 
     render (){
         return(
             <header className='header'>
-                <div className="container flex container">
+                <div className="container flex-container">
                 <h1>Recipe Box</h1>
+                    <AddButton
+                        toggleHidden={this.toggleHidden.bind(this)}
+                    />
                 </div>
             </header>  
         );
