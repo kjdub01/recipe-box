5050fa7f25db0aaec00547231dfd618b9b71e035
kjdub01 <kjewettwilliams@gmail.com>
2020-06-06 19:40:19 -0700

    create Toggle Render Props Children for togging in the app

---
+++ client/src/ToggleRPC.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Toggle extends React.Component {
+
+    state = {
+        on: false,
+    }
+
+    toggle = () => {
+        this.setState({
+            on: !this.state.on
+        })
+    }
+
+    render() {
+        const { children } = this.props;
+        return children({
+            on: this.state.on,
+            toggle: this.toggle,
+        });  
+    }
+}
+
+export default Toggle;
