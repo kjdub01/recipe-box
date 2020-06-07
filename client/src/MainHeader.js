a90602c25cd036fdc6e11fe75d2e5326dbf37121
kjdub01 <kjewettwilliams@gmail.com>
2020-06-06 19:43:53 -0700

    edit code to use ToggleRPC component for toggling

--- client/src/MainHeader.js
+++ client/src/MainHeader.js
@@ -2,32 +2,23 @@ import React from 'react'
 //import styles from './App.scss'
 import AddButton from './AddButton.js'
 import AddRecipe from './AddRecipe.js'
+import Toggle from './ToggleRPC.js'
 
 class MainHeader extends React.Component {
 
-    constructor(props) {
-        super(props) 
-        this.state = { isEmptyState: true}
-    }
-    
-    toggleHidden = () => {
-       this.setState({
-           ...this.state,
-           isEmptyState: false,
-           isToggleHidenState: true
-       })
-    }
-
     render (){
         return(
-            <header className='header'>
-                <div className="container flex-container">
-                <h1>Recipe Box</h1>
-                    {this.state.isEmptyState && <AddButton toggleHidden={this.toggleHidden} />}
-
-                    {this.state.isToggleHidenState && <AddRecipe />}
-                </div>
-            </header>  
+            <Toggle>
+                {({ on, toggle }) => (
+                    <header className='header'>
+                        <div className="container flex-container">
+                            <h1>Recipe Box</h1>
+                            {on && <AddRecipe toggle={toggle} on={on}/>}
+                            <button className="action pull-right" onClick={toggle}>Add Recipe</button>   
+                        </div>
+                    </header> 
+                )}
+            </Toggle> 
         );
     }    
 }
