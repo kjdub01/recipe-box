9ff96e78f2960d64f4941d0b81336704884e865f
kjdub01 <kjewettwilliams@gmail.com>
2020-06-06 19:48:30 -0700

    delete unnecessary files

--- client/src/AddButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const AddButton = props => {
-
-    return <button className="action pull-right" onClick={props.toggleHidden}>Add Recipe</button>
-}
-
-export default AddButton;
