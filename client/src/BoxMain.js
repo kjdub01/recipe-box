097523b01c3247e4b0e8e93747d1e3530cc898cd
kjdub01 <kjewettwilliams@gmail.com>
2020-06-13 18:54:23 -0700

    list ingredients

--- client/src/BoxMain.js
+++ client/src/BoxMain.js
@@ -3,15 +3,24 @@ import React from 'react'
 class BoxMain extends React.Component {
 
     render () {
+        const ingredients = this.props.ingredients,
+        listIngredients = ingredients.map((ingredient) => {
+            return(
+                <ul className='box_ingredients'>
+                    <li>{ingredient.ingredient_name}</li>
+                </ul>
+            )
+        })
+
         return(
             <main className='box_main'>
                 <div className='row'>
                     <div className='column six'>
                         <h3 className='box_subheading'>Ingredients</h3>
-                        <p className='box_ingrdients'>{this.props.ingredients}</p>
+                        {listIngredients}   
                     </div>
                     <div className='column six'>
-                        <img scr={this.props.recipeImg} className='box_img' />
+                        <img src={this.props.recipeImg} className='box_img' />
                     </div>
                     <h3>Directions</h3>
                     <p className='box_directions'>{this.props.recipeDirections}</p>
@@ -22,3 +31,5 @@ class BoxMain extends React.Component {
 }
 
 export default BoxMain;
+
+//
