70f2a5d8764f8143b5fc3611f19c33ef33d3425d
kjdub01 <kjewettwilliams@gmail.com>
2020-06-30 20:08:09 -0700

    git rid of old comments

--- client/src/components/AddRecipe.js
+++ client/src/components/AddRecipe.js
@@ -30,37 +30,6 @@ const AddRecipe = (props) => {
             </div>
               
         )
-/*
-    let formFields = {}
-
-    return(
-        <div className="form-bg-overlay">
-            <div className="form-bg">
-                <form classsName="form"> 
-                    onSubmit={ (e) => {
-                    e.preventDefault();
-                    props.handleFormSubmit(
-                        formFields.recipe_name.value,
-                        formFields.recipe_description.value,
-                        formFields.recipe_direction.value,
-                        formFields.img.value
-                    );
-                    e.target.reset(); }
-                    }>
-                        <i className="zmdi zmdi-class zmdi-hc-2x pull-right"
-                            OnClick = {this.props.toggleHidden} />
-                            <h2 className="form_title">Add Recipe</h2>
-                            <input ref={input => formFields.recipe_name = input} placeholder= 'Enter Recipe Name' />
-                            <input ref={input => formFields.recipe_description = input} placeholder= "Enter Recipe Description" />
-                            <input ref={input => formFields.recipe_direction = input} placeholder= "Enter Recipe Directions" />
-                            <input ref={input => formFields.img = input} placeholder= "Enter Recipe Image URL" />
-                            <button>Submit</button>
-                </form>
-            </div>
-        </div>
-    )
-
-    */
 }
 
 export default AddRecipe;
