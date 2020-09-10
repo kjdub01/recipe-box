3ebb1635460dfe485b1d738810dec09ccd612c4d
kjdub01 <kjewettwilliams@gmail.com>
2020-09-09 19:16:44 -0700

    add handleChange logic and update form styling

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,32 +1,30 @@
 import React from 'react'
 
 class SignIn extends React.Component {
-    state = {}
+    state = {
+        email: '',
+        password: ''
+    }
 
     handleChange = (e) => {
-        console.log(e)
+        this.setState ({
+            [e.target.id]: e.target.value
+        })
     }
 
     handleSubmit = (e) => {
-        console.log(e)
+        e.preventDefault();
+        console.log(this.state)
     }
     render() {
         return (
             <div className="form-bg-overlay"> 
                 <div className="form-bg">
-                    <form onsubmit={this.handleSubmit} className="form">
+                    <form onSubmit={this.handleSubmit} className="form">
                         <h2 className="form_title">Sign In</h2>
-                        <div className="form">
-                            <label htmlFor="email">Email    </label>
-                            <input type="email" id="email" onChange={this.handleChange} />
-                        </div>
-                        <div className= "form">
-                            <label htmlFor="password">Password  </label>
-                            <input type="password" id="password" onChange={this.handleChange}/>
-                        </div>
-                        <div className="form">
+                            <input type="email" id="email" onChange={this.handleChange} placeholder="Email"/>
+                            <input type="password" id="password" onChange={this.handleChange} placeholder="Password"/>
                             <button className="btn-primary">Login</button>
-                        </div>
                     </form>
                 </div>
             </div>
