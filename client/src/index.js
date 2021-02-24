33ee9538b1af5b3d8cad325e11a90df80c77ec0c
kjdub01 <kjewettwilliams@gmail.com>
2021-02-23 21:24:27 -0700

    update router with switch call

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AddRecipe from './components/recipes/AddRecipe';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp'
+import Recipe from './components/recipes/Recipe'
 import './index.css';
-
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 import * as serviceWorker from './serviceWorker';
-import AddRecipe from './components/recipies/AddRecipe';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
+
+
+
 
 const store = createStore(rootReducer, applyMiddleware(thunk)); /* add reducer here */
 
+
 ReactDOM.render(
     <Provider store={store}>
-    <Router>
-      <Route exact path="/" component={App} />
-      <Route path="/add-recipe" component={AddRecipe} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
-    </Router>
+      <Router >
+        <Switch>
+          <Route path="/add-recipe" component={AddRecipe} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/recipes/:recipeId" component={Recipe} />
+          <Route path="/" component={App} />
+        </Switch>
+      </Router>
     </ Provider>,
     document.getElementById('root')
 );
