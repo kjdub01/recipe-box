e22325bdd07cb45474f1bb429b599b2764fc6aba
kjdub01 <kjewettwilliams@gmail.com>
2021-02-23 21:14:05 -0700

    fix post request

--- client/src/actions/createRecipe.js
+++ client/src/actions/createRecipe.js
@@ -1,6 +1,18 @@
 export const createRecipe = (recipe) => {
     return (dispatch, getState) => {
         //async call to database
-      dispatch({ type: 'CREATE_RECIPE', recipe });
+        return fetch('http://localhost:3001/recipes', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application.json'
+          },
+          body: JSON.stringify({recipe: recipe})
+        })
+          .then (response => response.json())
+          .then(recipe => {
+            dispatch({ type: 'CREATE_RECIPE', recipe });
+        }).catch((err) => {
+          dispatch({ type: 'CREATE_RECIPE_ERROR', err})
+        })
     }
   };
