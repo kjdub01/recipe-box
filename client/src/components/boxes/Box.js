054bbfe7b799c5a309423a864be412e1d1f8d984
kjdub01 <kjewettwilliams@gmail.com>
2021-02-23 21:12:51 -0700

    give up on modal

--- client/src/components/boxes/Box.js
+++ client/src/components/boxes/Box.js
@@ -1,30 +1,84 @@
 import React from 'react'
-import Toggle from '../ToggleRPC.js'
 import BoxHeader from './BoxHeader.js'
 import BoxMain from './BoxMain.js'
 import BoxFooter from './BoxFooter.js'
 
-class Box extends React.Component {
 
-    render() {
-        return( 
-                <div className='box-bg-overlay'>
-                    <div className='box'>
-                        <BoxHeader recipeName={this.props.recipeName} toggle={this.props.toggle} /> 
-                        <BoxMain 
-                            ingredients={this.props.ingredients}
-                            recipeDirections={this.props.recipeDirections}
-                            recipeDescription={this.props.recipeDescription}
-                            recipeImg={this.props.recipeImg}
-                        />
-                        <BoxFooter 
-                            //delete={this.props.delete}
-                            //toggleEdit={this.props.edit}
-                        />
-                    </div>  
+class Box extends React.Component {
+    render(){    
+        return(
+            <div className='box-bg-overlay'>  
+                <div className='box'> 
+                    <BoxHeader 
+                        recipeName = {this.props.recipeName}
+                    /> 
+                    <BoxMain 
+                        ingredients={this.props.ingredients}
+                        recipeDirections={this.props.recipeDirections}
+                        recipeDescription={this.props.recipeDescription}
+                        recipeImg={this.props.recipeImg}
+                    />
+                    <BoxFooter
+                        //recipeId={this.recipe.id} 
+                        //delete={this.props.delete}
+                        //toggleEdit={this.props.edit}
+                    />
                 </div>
+            </div>  
         )
     }
 }
 
+
 export default Box;
+//recipeName={this.props.recipeName} toggle={this.props.toggle}
+
+/*
+
+
+
+ingredients={this.props.ingredients}
+recipeDirections={this.props.recipeDirections}
+recipeDescription={this.props.recipeDescription}
+recipeImg={this.props.recipeImg}
+ onClose() {
+        console.log(this.props)
+        this.props.hideModal();
+    }
+
+    render() {{
+        return( 
+            <Modal onClose={this.onClose}>
+                <BoxHeader  recipeName ={this.props.recipeName}/>
+
+                <BoxFooter
+                    //recipeId={this.recipe.id} 
+                    //delete={this.props.delete}
+                    //toggleEdit={this.props.edit}
+                /> 
+            </Modal>  
+        )
+        }
+    }
+
+
+    <div className='box-bg-overlay' key={this.props.recipeId}>
+                <div className='box'>   
+                    <BoxHeader 
+                        recipeName = {this.props.recipeName}
+                        toggle = {this.props.toggle} /> 
+                    <BoxMain 
+                        ingredients={this.props.ingredients}
+                        recipeDirections={this.props.recipeDirections}
+                        recipeDescription={this.props.recipeDescription}
+                        recipeImg={this.props.recipeImg}
+                    />
+                    <BoxFooter
+                        //recipeId={this.recipe.id} 
+                        //delete={this.props.delete}
+                        //toggleEdit={this.props.edit}
+                    />
+                </div>  
+            </div>  
+           
+ */
--- client/src/components/boxes/BoxMain.js
+++ client/src/components/boxes/BoxMain.js
@@ -3,31 +3,31 @@ import React from 'react'
 class BoxMain extends React.Component {
 
     render () {
-        const ingredients = this.props.ingredients,
-        listIngredients = ingredients.map((ingredient, index) => {
-            return(
-                <span key={index}>{ (index ? ', ' : '') + ingredient.ingredient_name}</span> 
-            )
-        });
-
+        console.log(this.props)
+        //const ingredients = this.props.ingredients,
+        //listIngredients = ingredients.map((ingredient, index) => {
+            //return(
+             //   <span key={index}>{ (index ? ', ' : '') + ingredient.ingredient_name}</span> 
+           // )
+        //});
         return(
             <main className='box_main'>
                 <div className='row'>
                     <div className='column six'>
                         <h3 className='box_subheading'>Ingredients</h3>
                         <p>
-                            {listIngredients}
+                            ingredients
                         </p> 
                     </div>
                     <div className='column six'>
-                        <img src={this.props.recipeImg} className='box_img' />
+                        <img src="https://cocoaandlavender.com/wp-content/uploads/2019/04/DSABioPicBWsmall.jpg" className='box_img' />
                     </div>
                 </div>
                 <h3 className=''>Directions</h3>
-                <p className='box_directions'>{this.props.recipeDirections}</p>
+                <p className='box_directions'>directions</p>
                 <h3 className=''>My Thoughts</h3>
-                <p className='box_directions'>{this.props.recipeDescription}</p>
-                
+                <p className='box_directions'>description</p>
+        
             </main>
         )
     }
@@ -35,4 +35,9 @@ class BoxMain extends React.Component {
 
 export default BoxMain;
 
-//
+//{listIngredients}
+//listIngredients = ingredients.map((ingredient, index) => {
+            //return(
+                //<span key={index}>{ (index ? ', ' : '') + ingredient.ingredient_name}</span> 
+           // )
+        //});
