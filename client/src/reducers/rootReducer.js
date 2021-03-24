b9a2c742794229d4dda3b37ae04b7dc3fcc359d4
kjdub01 <kjewettwilliams@gmail.com>
2021-03-24 14:37:35 -0700

    update state to have recipes not recipe

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -4,7 +4,7 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    recipe: reciepeReducer
+    recipes: reciepeReducer
 });
 
 export default rootReducer
