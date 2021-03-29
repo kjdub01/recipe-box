004cf32dc9205d7e07e7beb896190f4cdf8b1dff
kjdub01 <kjewettwilliams@gmail.com>
2021-03-29 16:49:35 -0700

    combine actions into one recipeActions file

--- client/src/actions/fetchRecipes.js
+++
@@ -1,8 +0,0 @@
-export function fetchRecipes() {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_RECIPES' });
-    fetch('http://localhost:3001/recipes')
-      .then(response => response.json())
-      .then(recipes => dispatch({ type: 'ADDING_RECIPES', recipes }))
-    };
-  }
---
+++ client/src/actions/recipeActions.js
@@ -0,0 +1,21 @@
+export function fetchRecipes() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_RECIPES' });
+    fetch('http://localhost:3001/recipes')
+      .then(response => response.json())
+      .then(recipes => dispatch({ type: 'ADDING_RECIPES', recipes }))
+    };
+  }
+
+  export const createRecipe = recipe => {
+    return dispatch => {
+      fetch ('http://localhost:3001/recipes', {
+        method: 'POST',
+        body: JSON.stringify({recipe: recipe}),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then(response => response.json())
+      .then(recipe => dispatch({ type: 'CREATE_RECIPE', recipe }))
+    }
+  }
+    
