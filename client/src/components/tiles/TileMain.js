ba87b45d2dc0ec12236928b64e5a519bb1492bfa
kjdub01 <kjewettwilliams@gmail.com>
2021-04-06 16:43:53 -0700

    update component access of props

--- client/src/components/tiles/TileMain.js
+++ client/src/components/tiles/TileMain.js
@@ -3,11 +3,11 @@ import React from 'react'
 class TileMain extends React.Component {
     render() {
         let heroDiv;
-        if (this.props.recipeImg) {
+        if (this.props.recipe.img) {
             heroDiv = <div
                 className="tile_hero"
                 style={{
-                    background: "url(" + this.props.recipeImg + ")", 
+                    background: "url(" + this.props.recipe.img + ")", 
                     backgroundSize: "100% auto",
                     backgroundPosition: "center center",	
                     textDecoration: "none"
@@ -22,7 +22,7 @@ class TileMain extends React.Component {
             <main className='tile_main'>
                 {heroDiv}
                 <header className='tile_header'>
-                    <h2 className="tile_title">{this.props.recipeName}</h2>
+                    <h2 className="tile_title">{this.props.recipe.name}</h2>
                 </header>
             </main>
         ); 
