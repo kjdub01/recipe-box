f2d44e32576b0e6430a3dbab0a89a4fb119baaca
kjdub01 <kjewettwilliams@gmail.com>
2021-04-06 16:37:42 -0700

    rewrite as a functional component

--- client/src/components/tiles/Tile.js
+++ client/src/components/tiles/Tile.js
@@ -3,35 +3,21 @@ import TileMain from './TileMain'
 import TileFooter from './TileFooter'
 import { Link } from "react-router-dom";  
 
-class Tile extends React.Component {
-
-    render() {
-        const recipes = this.props.recipes,
-            tileNodes = recipes.map((recipe) => {
-                return(
-                   
+const Tile = ({recipes}) => {
+    return (
+        <div className="tile-container"> 
+                { recipes && recipes.map(recipe => {
+                    return(
                         <div className="tile" key={recipe.id}>
-                            <Link style={{ textDecoration: 'none' }} to={"/recipes/" + recipe.id}>
-                            <TileMain
-                                recipeName={recipe.name}
-                                recipeImg={recipe.img}
-                             />
-                            </Link> 
-
-                            <TileFooter
-                                recipe={recipe} 
-                                recipeId={recipe.id}   
-                            />   
+                        <Link style={{ textDecoration: 'none' }} to={"/recipes/" + recipe.id}> 
+                            <TileMain recipe={recipe} key={recipe.id}/>
+                            <TileFooter />
+                        </Link>
                         </div>
-                       
-            );
-        })
-        return (
-            <main className="tile-container">
-                {tileNodes}
-            </main>
-        )
-    }
+                    )
+                })}
+        </div>
+    )
 }
 
 export default Tile
