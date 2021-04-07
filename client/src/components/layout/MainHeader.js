25f5584161e73bea042fd6c8520d00426d869990
kjdub01 <kjewettwilliams@gmail.com>
2021-04-06 20:45:17 -0700

    rewrite mainheader as a functional component and add navbarLinks

--- client/src/components/layout/MainHeader.js
+++ client/src/components/layout/MainHeader.js
@@ -1,25 +1,16 @@
 import React from 'react'
-//import styles from './App.scss'
-import AddRecipe from '../recipes/AddRecipe.js'
-import Toggle from '../ToggleRPC.js'
 import { Link } from 'react-router-dom'
+import NavbarLinks from './NavbarLinks'
 
-class MainHeader extends React.Component {
-
-    render (){
-        return(
-            <header className='header'>
-                <div className="container flex-container">
-                    <Link style={{ textDecoration: 'none', color: 'white' }} to='/recipes'>
-                        <h1>Recipe Box</h1>
-                    </Link>
-                    <Link to='/add-recipe'>
-                        <button className="action pull-right btn-primary" >Add Recipe</button>
-                    </Link>   
-                </div>
-            </header> 
-        );
-    }    
+const MainHeader = () => {
+    return (
+        <nav className='header'>
+            <div className="container flex-container">
+                <Link style={{ textDecoration: 'none', color: 'white' }} className="brand-logo" to='/recipes'><h1>Recipe Box</h1></Link>
+                <NavbarLinks />
+            </div>
+        </nav>
+    )
 }
 
 export default MainHeader; 
---
+++ client/src/components/layout/NavbarLinks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const NavbarLinks = () => {
+    return (
+        <button className="action pull-right btn-primary">
+            <NavLink to='/add-recipe' style={{ textDecoration: 'none', fontWeight: 'bold' }}>Add Recipe</NavLink>
+        </button> 
+    )
+}
+
+export default NavbarLinks;
