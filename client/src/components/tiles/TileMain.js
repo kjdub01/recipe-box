20dea2f909d4426e7027fa15f171cb6733c07119
kjdub01 <kjewettwilliams@gmail.com>
2021-04-06 18:01:36 -0700

    rewrite as functional component

--- client/src/components/tiles/TileMain.js
+++ client/src/components/tiles/TileMain.js
@@ -1,32 +1,29 @@
 import React from 'react'
 
-class TileMain extends React.Component {
-    render() {
-        let heroDiv;
-        if (this.props.recipe.img) {
-            heroDiv = <div
-                className="tile_hero"
-                style={{
-                    background: "url(" + this.props.recipe.img + ")", 
-                    backgroundSize: "100% auto",
-                    backgroundPosition: "center center",	
-                    textDecoration: "none"
+const TileMain = ({recipe}) => {
+    let heroDiv
+    switch(recipe.img === null) {
+        default:
+        case true:
+            heroDiv = <div className = 'title_hero' /> 
+            break;
+        case false:
+            heroDiv = <div className="tile_hero" style={{ 
+                background: "url(" + recipe.img + ")", 
+                backgroundSize: "100% auto",
+                backgroundPosition: "center center",	
+                textDecoration: "none"
                 }}
             />
-        } else {
-            heroDiv = <div
-                className = 'title_hero'
-                />
-        }
-        return (
-            <main className='tile_main'>
-                {heroDiv}
-                <header className='tile_header'>
-                    <h2 className="tile_title">{this.props.recipe.name}</h2>
-                </header>
-            </main>
-        ); 
-    }
+    } 
+    return (
+    <main className='tile_main'>
+        {heroDiv}
+        <header className='tile_header'>
+            <h2 className="tile_title">{recipe.name}</h2>
+        </header>
+    </main> 
+   ) 
 }
 
 export default TileMain;
