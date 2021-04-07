17e0f9c1613759db795dba59d4c9f5ce40853038
kjdub01 <kjewettwilliams@gmail.com>
2021-04-06 18:06:40 -0700

    rewrite as functional component

--- client/src/components/recipes/RecipeHeader.js
+++ client/src/components/recipes/RecipeHeader.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
-class RecipeHeader extends React.Component {
-
-    render() {
-        return(
-            <header className="title-container">
-                <h1 className="recipe-title">{this.props.recipeName}</h1>
-            </header>
-        )
-    }
+const RecipeHeader = ({recipeName}) => {
+    return (
+        <header className="title-container">
+                <h1 className="recipe-title">{recipeName}</h1>
+        </header>
+    )
 }
 
 export default RecipeHeader
