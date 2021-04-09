f4ed3f81945453fa1e0a84f8229a5481c7d4d7ba
kjdub01 <kjewettwilliams@gmail.com>
2021-04-08 17:24:21 -0700

    add and configure comment model and update recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :user_recipes
-  has_many :users, through: :user_recipes
+  has_many :comments
   has_many :ingredients
   accepts_nested_attributes_for :ingredients, allow_destroy: true
 end
