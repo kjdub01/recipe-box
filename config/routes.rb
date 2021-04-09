a8fb57cfe0e83fb7f8f39b19a68dca9bc4546abc
kjdub01 <kjewettwilliams@gmail.com>
2021-04-08 17:25:11 -0700

    add comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #resources :ingredients
   root to: 'application#index'
+  resources :comments, only: [:index, :new, :create, :destroy]
   resources :recipes
     resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
