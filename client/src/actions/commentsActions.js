8a855f09040a7018c4696295a676f0b280f5547a
kjdub01 <kjewettwilliams@gmail.com>
2021-04-13 15:20:34 -0700

    add post request to comments actions

--- client/src/actions/commentsActions.js
+++ client/src/actions/commentsActions.js
@@ -5,3 +5,15 @@ export const fetchComments = () => {
         .then(comments => dispatch({ type: "FETCH_COMMENTS", payload: comments}))
     }
 }
+
+export const createComment = comment => {
+    return dispatch => {
+      fetch ('http://localhost:3001/comments', {
+        method: 'POST',
+        body: JSON.stringify({comment: comment}),
+        headers: { 'Content-Type': 'application/json',}
+      })
+      .then(response => response.json())
+      .then(comment => dispatch({ type: 'CREATE_COMMENT', comment }))
+    }
+  }
