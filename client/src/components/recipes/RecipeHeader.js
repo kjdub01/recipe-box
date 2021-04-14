d0f06167671b39a63c3cbecd2522bca98602c94b
kjdub01 <kjewettwilliams@gmail.com>
2021-04-14 16:44:38 -0700

    add delete button to header of recipe pages

--- client/src/components/recipes/RecipeHeader.js
+++ client/src/components/recipes/RecipeHeader.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const RecipeHeader = ({recipeName}) => {
     return (
-        <header className="title-container">
+        <header>
+            <div className="title-container">
                 <h1 className="recipe-title">{recipeName}</h1>
+            </div>
+            <div className="recipe-controls-primary">
+                <button className="btn-warning">Delete Recipe</button>
+            </div>
         </header>
     )
 }
--- client/src/components/recipes/RecipeMain.js
+++ client/src/components/recipes/RecipeMain.js
@@ -5,10 +5,14 @@ const RecipeMain = ({recipe}) => {
     return (
         <div className="article">
             <div className="recipe-intro">
-                <p className="recipe-description">{recipe.description}</p>
-            <div className="recipe-img-container">
-                <img className="recipe-img" src={recipe.img} />
-            </div>
+                <div className="recipe-img-container">
+                    <picture>
+                        <img className="recipe-img" src={recipe.img} />
+                    </picture>
+                </div>
+                <div className="top-note">
+                    <p className="recipe-description">{recipe.description}</p>
+                </div>
             </div>
             <div className="recipe-instructions">
                 <section className="ingredients-wrap">
