44d56424b6464b5d3d924c00870d050c7dacc1aa
kjdub01 <kjewettwilliams@gmail.com>
2021-04-20 17:52:49 -0700

    add dependency to destroy comments when recipe is destroyed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy_async
   has_many :ingredients
   accepts_nested_attributes_for :ingredients, allow_destroy: true
 end
