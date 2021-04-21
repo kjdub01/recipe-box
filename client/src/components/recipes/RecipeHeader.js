b269a2c53fd5eafff4e24c6743664fee5f164b44
kjdub01 <kjewettwilliams@gmail.com>
2021-04-20 18:12:06 -0700

    add handleClick function and use connect higher order function to handle redux delete request

--- client/src/components/recipes/RecipeHeader.js
+++ client/src/components/recipes/RecipeHeader.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteRecipe } from '../../actions/recipeActions'
 
-const RecipeHeader = ({recipeName}) => {
+const RecipeHeader = (props) => {
+    const handleClick = () => {
+        props.deleteRecipe(props.recipe.id)
+        props.goBack()
+    }
+
     return (
         <header>
             <div className="title-container">
-                <h1 className="recipe-title">{recipeName}</h1>
+                <h1 className="recipe-title">{props.recipe.name}</h1>
             </div>
             <div className="recipe-controls-primary">
-                <button className="btn-warning">Delete Recipe</button>
+                <button className="btn-warning" onClick={handleClick}>Delete Recipe</button>
             </div>
         </header>
     )
 }
 
-export default RecipeHeader
+export default connect(null, { deleteRecipe })(RecipeHeader)
