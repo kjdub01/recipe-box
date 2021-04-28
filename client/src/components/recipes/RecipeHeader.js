f96a9693e1b09ecc66c893db5396477485478dd5
kjdub01 <kjewettwilliams@gmail.com>
2021-04-28 15:21:45 -0700

    git rid of connect and deleteRecipe action imports, get rig of unnecessary handleClick function, add Link

--- client/src/components/recipes/RecipeHeader.js
+++ client/src/components/recipes/RecipeHeader.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { deleteRecipe } from '../../actions/recipeActions'
+import { Link } from 'react-router-dom'
 
 const RecipeHeader = (props) => {
-    const handleClick = () => {
-        props.deleteRecipe(props.recipe.id)
-        props.goBack()
-    }
-
     return (
         <header>
             <div className="title-container">
                 <h1 className="recipe-title">{props.recipe.name}</h1>
             </div>
             <div className="recipe-controls-primary">
-                <button className="btn-warning" onClick={handleClick}>Delete Recipe</button>
+                <Link to='/'>
+                <button className="btn-warning" onClick={() => {props.deleteRecipe(props.recipe.id)}}>Delete Recipe</button>
+                </Link>
             </div>
         </header>
     )
 }
 
-export default connect(null, { deleteRecipe })(RecipeHeader)
+export default RecipeHeader
