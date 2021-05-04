3c1e637a18d2e407b8d0ad38e198319c81e39fcb
kjdub01 <kjewettwilliams@gmail.com>
2021-05-04 15:32:30 -0700

    create ui reducer for new state structure

---
+++ client/src/reducers/uiReducer.js
@@ -0,0 +1,28 @@
+export const uiReducer = (state = [], action) => {
+    switch(action.type) {
+
+        case 'LOADING_RECIPES':
+            return {
+                loading: true
+            }
+        
+        case 'FETCH_RECIPES':
+            return {
+                loading: false
+            }
+
+        case 'CREATE_RECIPE':
+            return {
+                loading: false
+            }
+
+        case 'DELETE_RECIPES_SUCCESS':
+            return {
+                deleted: true
+            }
+                
+            
+        default:
+            return state;
+    }
+};
