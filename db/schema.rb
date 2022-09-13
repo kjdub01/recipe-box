95628b97b5fdb89884f84fc371c42c6352da8aa9
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2022-09-13 16:24:24 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_01_211623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "content"
